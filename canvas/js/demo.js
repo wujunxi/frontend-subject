8e04d964464e2fd27857c9d029220b2e35b94e8e
xn066417 <xn066417@xiaoniu.com>
2017-05-05 17:55:50 +0800

    update canvas demo

---
+++ canvas/js/demo.js
@@ -0,0 +1,84 @@
+// Class Define
+
+function KV(k, v) {
+    this.key = k;
+    this.val = v;
+}
+
+function Action() {
+    this.actionType = "";
+}
+
+Action.prototype.MOVE = new KV(0, "移动");
+Action.prototype.PEN = new KV(1, "画笔");
+Action.prototype.ERASER = new KV(2, "橡皮擦");
+Action.prototype.COLOR = new KV(3, "颜色");
+Action.prototype.SHAPE = new KV(4, "形状");
+
+
+function main() {
+    var cvs = document.getElementById("canvasMain");
+    if (!cvs.getContext) return;
+    var ctx = cvs.getContext("2d");
+    drawTest(ctx);
+    var isMouseDown = false;
+    $("#canvasMain").on("mousemove", function(e) {
+        // log(e.offsetX, e.offsetY);
+        $("[data-name=position]").text(e.offsetX + "," + e.offsetY);
+        if (isMouseDown) {
+
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.stroke();
+        }
+    }).on("mousedown", function(e) {
+        log("mousedown");
+        isMouseDown = true;
+        ctx.beginPath();
+        ctx.moveTo(e.offsetX, e.offsetY);
+    }).on("mouseup", function(e) {
+        log("mouseup");
+        isMouseDown = false;
+        // ctx.endPath();
+    });
+    $("body").on("click", "[data-oper]", function() {
+        var $this = $(this),
+            operName = $this.attr("data-oper");
+        if (operName == "clear") {
+            ctx.clearRect(0, 0, cvs.width, cvs.height);
+        } else if (operName == "pick_color") {
+            var color = $this.attr("data-val");
+            ctx.fillStyle = color;
+            ctx.strokeStyle = color;
+        } else if (operName == "draw_shape") {
+
+        }
+    });
+}
+
+function drawTest(ctx) {
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect(10, 10, 55, 50);
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(30, 30, 55, 50);
+}
+
+function log(text) {
+    console.log(text);
+}
+
+/*
+1.绘制矩形
+fillRect(x, y, width, height)
+绘制一个填充的矩形
+strokeRect(x, y, width, height)
+绘制一个矩形的边框
+clearRect(x, y, width, height)
+清除指定矩形区域，让清除部分完全透明。
+
+2.颜色填充
+fillStyle = color
+设置图形的填充颜色。
+strokeStyle = color
+设置图形轮廓的颜色。
+*/
