415c259ecd6e3869aa6e4367f6a1c68fb16c4221
wujunxi <347348453@qq.com>
2017-05-17 00:13:24 +0800

    first add my-paint

---
+++ my-paint/src/MyPaint.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <layer :width="600" :height="600"></layer>
+    </div>
+</template>
+
+<script>
+import Layer from './components/Layer'
+
+export default {
+    name: 'myPaint',
+    data: function () {
+        return {}
+    },
+    components: {
+        Layer
+    }
+}
+</script>
+
+<style>
+body {
+    background: #333;
+}
+
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+    background: url(assets/bg.svg);
+    background-size:20px 20px;
+    position:absolute;
+    top:50%;
+    left:50%;
+    transform: translate(-50%,-50%);
+}
+</style>
---
+++ my-paint/src/components/Layer.vue
@@ -0,0 +1,15 @@
+<template>
+    <canvas>
+    </canvas>
+</template>
+
+<script>
+export default {
+  name: 'layer',
+  prop: ['width','height']
+}
+</script>
+
+<style scoped>
+
+</style>
