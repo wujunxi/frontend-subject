ef17242e4abd9ec6544a7c74402ac057b5f86e73
wujunxi <347348453@qq.com>
2017-05-18 23:27:13 +0800

    finish simple paging

--- simple paging/js/paging.js
+++ simple paging/js/paging.js
@@ -8,14 +8,9 @@
 
         var htmlStr = "";
         htmlStr += '<ul class="paging">';
-        htmlStr += '<li class="pg-btn _first">首页</li>';
+        htmlStr += '<li class="pg-btn _first" data-num="1">首页</li>';
         htmlStr += '<li class="pg-btn _prev">上一页</li>';
         htmlStr += '<li class="pg-txt _before">...</li>';
-        htmlStr += '<li class="pg-btn _num">12</li>';
-        htmlStr += '<li class="pg-btn _num on">13</li>';
-        htmlStr += '<li class="pg-btn _num">14</li>';
-        htmlStr += '<li class="pg-btn _num">15</li>';
-        htmlStr += '<li class="pg-btn _num">16</li>';
         htmlStr += '<li class="pg-txt _after">...</li>';
         htmlStr += '<li class="pg-btn _next">下一页</li>';
         htmlStr += '<li class="pg-btn _last">尾页</li>';
@@ -32,50 +27,67 @@
             $next = $("._next", $box),
             $last = $("._last", $box);
 
-        $box.on("click",".pg-btn",function(){
-            var $this = $(this);
-            if($this.hasClass("on") || $this.hasclass("off")){
+        $box.on("click", "[data-num]", function() {
+            var $this = $(this),
+                num = parseInt($this.attr("data-num"));
+            if ($this.hasClass("on") || $this.hasClass("off")) {
                 return;
             }
-            if($this.hasClass("_num")){
-                opt.onChangePage(parseInt($this.text()));
-            }else if($this.hasClass("_first")){
-
-            }
+            opt.onChangePage(num);
         });
 
         return {
-            refresh: function(cur, size, total) {
+            refresh: function(_cur, size, total) {
+                var cur = parseInt(_cur);
+                // 计算总页数
                 var totalPage = Math.ceil(total / size);
+                // 判断是否显示省略号
                 $before.toggle(cur > 3);
                 $after.toggle(cur < totalPage - 2);
+                // 设置上一页和首页是否可点击
                 if (cur == 1) {
                     $prev.add($first).addClass("off");
                 } else {
                     $prev.add($first).removeClass("off");
                 }
+                // 设置下一页和尾页是否可点击
                 if (cur == totalPage) {
                     $next.add($last).addClass("off");
                 } else {
                     $next.add($last).removeClass("off");
                 }
-                var from = cur -2,
-                to = cur + 2;
-                from = from < 1 ? 1: from;
+                // 设置下一页、上一页、尾页页码
+                $prev.attr("data-num", cur - 1);
+                $next.attr("data-num", cur + 1);
+                $last.attr("data-num", totalPage);
+                // 设置可选页码范围
+                var from = cur - 2,
+                    to = cur + 2;
+                from = from < 1 ? 1 : from;
                 to = to > totalPage ? totalPage : to;
-                console.log(cur,from,to);
-                var newHtml = "",i;
-                for(i = from; i <= to; i++){
-                    newHtml += pageNum(i,i == cur);
+                var newHtml = "",
+                    i;
+                // 构造页码html
+                for (i = from; i <= to; i++) {
+                    newHtml += pageNum(i, i == cur);
                 }
-                $("._num",$box).remove();
+                // 移出老的页面
+                $("._num", $box).remove();
+                // 插入新页码
                 $before.after(newHtml);
             }
         };
     };
 
+    /**
+     * 构造页码按钮html
+     * 
+     * @param {any} num 页码
+     * @param {any} isOn 是否当前页码
+     * @returns 
+     */
     function pageNum(num, isOn) {
-        return '<li class="pg-btn _num' + (isOn ? ' on' : '') + '">' + num + '</li>'
+        return '<li class="pg-btn _num' + (isOn ? ' on' : '') + '" data-num="' + num + '">' + num + '</li>'
     }
 
 })(jQuery, window);
